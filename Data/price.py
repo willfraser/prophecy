4c9f5492262784a97a4c06793ed82c44bd61d44b
Will Fraser <fraser.will@gmail.com>
2017-08-29 02:34:48 +0000

    print price

--- Data/price.py
+++ Data/price.py
@@ -1,2 +1,3 @@
-import urllib.request
-urllib.request.urlopen("http://example.com/foo/bar").read()
+import urllib3.request
+var price = urllib3.request.urlopen("https://api.quadrigacx.com/v2/ticker?book=eth_cad").read
+print price 
