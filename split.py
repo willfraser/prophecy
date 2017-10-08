33007152bf54bf76a3994a380eefed5b2b7538df
Will Fraser <fraser.will@gmail.com>
2017-10-08 03:43:56 +0000

    Removed testing code

--- split.py
+++ split.py
@@ -99,8 +99,8 @@ def run(target_up,target_down, trans_fee,currencies, cryptos, k):
                         
                         #determine if bids or asks are volume limiting and only trade the smallest of the two 
                         #so we don't get stuck with extra 
-                        print(currency_1.ask_volume, "units being asked for in", currency_1.currency)
-                        print(currency_2.bid_volume, "units being bid for in", currency_2.currency)
+                        # print(currency_1.ask_volume, "units being asked for in", currency_1.currency)
+                        # print(currency_2.bid_volume, "units being bid for in", currency_2.currency)
                         
                         if((float(currency_1.ask_volume) - float(currency_2.bid_volume)) < 0):
                             volume = currency_1.ask_volume
@@ -110,7 +110,7 @@ def run(target_up,target_down, trans_fee,currencies, cryptos, k):
                         #triggers the buy action with a safety factor to again insure we don't get stuck with 
                         #extra currency
                         print("Buy", volume, "of", crypto.currency, "in", currency_1.currency, "and sell", currency_2.currency, "for a margin of", currency_1.upside_arbitrage[currency_2.currency])
-                        # kraken.buy_sell(volume, currency_1, currency_2, crypto.currency, 0.5,k)
+                        kraken.buy_sell(volume, currency_1, currency_2, crypto.currency, 0.5,k)
                     else:
                         #no trades found that meet our criteria
                         print("Hold", crypto.currency, currency_1.currency, "through", currency_2.currency, "upside margin is:", currency.upside_arbitrage[currency_2.currency])
