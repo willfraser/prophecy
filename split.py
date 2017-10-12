737207c890ead38402e9f9cf3d5c0f5720ee7bd3
Will Fraser <fraser.will@gmail.com>
2017-10-12 17:16:40 +0000

    added downside buy

--- split.py
+++ split.py
@@ -125,7 +125,7 @@ def run(target_up,target_down, trans_fee,currencies, cryptos, k):
                         #triggers the buy action with a safety factor to again insure we don't get stuck with 
                         #extra currency
                         print("Buy", volume, "of", crypto.currency, "in", currency_1.currency, "and sell", currency_2.currency, "for a margin of", currency_1.downside_arbitrage[currency_2.currency])
-                    #     kraken.buy_sell(volume,currency, USD, crypto.currency,0.5,k)
+                        kraken.buy_sell(volume,currency_1, currency_2, crypto.currency,0.8,k)
                     
                     # else:
                     #     #no trades found that meet our criteria
